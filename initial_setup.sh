1064152bbd9493a9b573c7c7bae913755b33cdf4
Rainer Gerhards <rgerhards@adiscon.com>
2015-06-14 05:58:53 -0400

    add initialization file

---
+++ initial_setup.sh
@@ -0,0 +1,6 @@
+echo This script sets up the rsyslog infrastructure
+cd ..
+echo cloning repositories
+git clone https://github.com/rsyslog/libestr.git 
+git clone https://github.com/rsyslog/liblognorm.git 
+git clone https://github.com/rsyslog/rsyslog.git 
