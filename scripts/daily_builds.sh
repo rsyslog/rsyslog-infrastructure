8e9258b6d8cdf6696cbc3ac6105541d570b9d226
Florian Riedl <friedl@adiscon.com>
2016-01-12 17:05:46 +0000

    Added script for libfastjson

--- scripts/daily_builds.sh
+++ scripts/daily_builds.sh
@@ -4,6 +4,7 @@ source $RSI_SCRIPTS/config.sh
 # we need to call different files for the subprojects. These are
 # mostly identical, but have important small differences. At a later
 # stage, we may want to unify this.
+$RSI_SCRIPTS/daily_tarball_libfastjson.sh
 $RSI_SCRIPTS/daily_tarball_libgt.sh
 $RSI_SCRIPTS/daily_tarball_libestr.sh
 $RSI_SCRIPTS/daily_tarball_liblogging.sh
---
+++ scripts/daily_tarball_libfastjson.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+source $RSI_SCRIPTS/config.sh 
+
+cd $INFRAHOME/repo/libfastjson
+make distclean
+git pull
+git checkout -f master
+if [ $? -ne 0 ]; then
+    git checkout master |& mutt -s "libfastjson tarball: git checkout failed!" $RS_NOTIFY_EMAIL
+    exit 1
+fi
+git pull
+if [ $? -ne 0 ]; then
+    git pull |& mutt -s "libfastjson tarball: git pull failed!" $RS_NOTIFY_EMAIL 
+       exit 1
+fi
+
+# we need to rename the version
+rm *.tar.gz
+sed s/\\.master\]/\\.`git log --pretty=format:'%H' -n 1|cut -c 1-12`\]/ < configure.ac > configure.ac.new
+mv configure.ac.new configure.ac
+
+autoreconf -fvi && ./configure --prefix=$INFRAHOME/local && make || exit $?
+
+echo trying make dist
+rm -rf *.tar.gz
+make dist
+if [ $? -ne 0 ]; then
+    make dist |& mutt -s "libfastjson tarball: make dist failed" $RS_NOTIFY_EMAIL 
+    exit 1
+fi
+
+# work-around fix permissions
+# TODO: how to handle script abort? Any way to avoid this work-around here?
+chmod -R g+w .
+chgrp -R infrastructure .
+
+# install in our local build environment
+make install
+
+# now update tarball on rsyslog
+TARFILE=`ls *.tar.gz`
+echo tarfile for upload: $TARFILE
+# TODO: we need to copy this file inside the local file system once we
+# know where this will be
+#scp -p $TARFILE download.rsyslog.com:/home/adisconweb/www/wordpress-mu/wp-content/blogs.dir/11/files/download/rsyslog/liblognorm-daily.tar.gz
+
+# reset version number changes
+git checkout -f master
